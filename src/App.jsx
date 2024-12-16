b6183d46aca6c1ba6f1248f156d28675f3a6f842
zhuchangjun <zhuchangjun@hotmail.com>
2024-12-15 21:05:52 -0500

    changes for amplify

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/*
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -33,3 +34,22 @@ function App() {
 }
 
 export default App
+*/
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+
+        <h1>Hello from Amplify</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
