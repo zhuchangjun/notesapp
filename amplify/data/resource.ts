894e762784a4fc558d78d6cb3e21cfba221d5aba
zhuchangjun <zhuchangjun@hotmail.com>
2024-12-26 23:34:05 -0500

    the notes app

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -5,7 +5,6 @@ The section below creates a Todo database table with a "content" field. Try
 adding a new "isDone" field as a boolean. The authorization rule below
 specifies that any unauthenticated user can "create", "read", "update", 
 and "delete" any "Todo" records.
-=========================================================================*/
 const schema = a.schema({
   Todo: a
     .model({
@@ -22,7 +21,28 @@ export const data = defineData({
     defaultAuthorizationMode: 'iam',
   },
 });
+=========================================================================*/
+//import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
+const schema = a.schema({
+  Note: a
+
+    .model({
+      name:a.string(),
+      description: a.string(),
+      image: a.string(),
+    })
+    .authorization((allow) => [allow.owner()]),
+});
+
+export type Schema = ClientSchema<typeof schema>;
+
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: 'userPool',
+  },
+});
 /*== STEP 2 ===============================================================
 Go to your frontend source code. From your client-side code, generate a
 Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
